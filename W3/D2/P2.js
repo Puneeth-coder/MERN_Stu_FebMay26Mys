5f040cdd825e9baf50ab1ebe2e0d5213e4d909f1
Puneeth-coder <puneethm240@gmail.com>
2026-03-03 22:12:55 +0530

    Comparision operators

---
+++ W3/D2/P2.js
@@ -0,0 +1,11 @@
+//Comparision operators
+console.log("Comparision operators");
+let x = 4, y = '3';
+console.log("x==y: ", x==y);   //Loose equality
+console.log("x===y: ", x===y); //strict equality
+console.log("x!=y ", x!=y);
+console.log("x!==y ", x!==y);
+console.log("x>y ", x>y);
+console.log("x<y ", x<y);
+console.log("x>=y ", x>=y);
+console.log("x<=y ", x<=y);
