aa8aaa1c9ce9765adeb93ca37cc89c24391f0ad9
Puneeth-coder <puneethm240@gmail.com>
2026-03-03 15:46:31 +0530

    Arithmetic operators

---
+++ W3/D2/P1.js
@@ -0,0 +1,14 @@
+// Arithmetic Operators
+const a = 2, b = 3;
+let c = 0;
++
+console.log("Arithmetic Operation");
+console.log("a+b:",a+b);
+console.log("a-b",a-b);
+console.log("a*b",a*b);
+console.log("a%b",a%b);  //modulas
+console.log("a**b",a**b); //exponential
+// console.log("a++",a++); // Post-increment
+// console.log("++a",++a);  // Pre-increment
+// console.log("a--",a--); // Post-decrement
+// console.log("--a",--a); // pre-decrement
