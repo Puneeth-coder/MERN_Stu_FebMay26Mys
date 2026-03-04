2a87cce0530166b9b5633ab5017abf94f3824c38
Puneeth-coder <puneethm240@gmail.com>
2026-03-04 11:13:53 +0530

    Function declaration and calling

---
+++ W3/D2/P5.js
@@ -0,0 +1,8 @@
+
+// Function declaration 
+function greet(name) {
+    console.log("Hello," +name+"i")
+}
+// call function
+greet("Puneeth ");     //hoisting is supported in the case of function
+
