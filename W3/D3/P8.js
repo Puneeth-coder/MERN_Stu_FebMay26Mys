e86f130635ed5658528b524141c70934a0f9d0bd
Puneeth-coder <puneethm240@gmail.com>
2026-03-04 14:36:02 +0530

    Adding JOSN parsing and JOSAN stringify

--- W3/D3/P8.js
+++ W3/D3/P8.js
@@ -2,6 +2,13 @@
 const employee = {
     id: 92,
     name: "Punee",
-    dept: "EEE",
+    dept: "CSE",
     isActive: true
 };
+const jsonstring = JSON.stringify(employee);
+console.log(jsonstring);
+console.log(employee);
+
+// JSON parsing
+const parsedObject = JSON.parse(jsonstring);
+console.log(parsedObject);
