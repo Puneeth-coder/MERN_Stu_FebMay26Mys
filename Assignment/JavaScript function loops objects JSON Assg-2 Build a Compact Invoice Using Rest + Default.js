35226e1c656d23d34450229a1495a7485366e30a
Puneeth-coder <puneethm240@gmail.com>
2026-03-17 17:29:57 +0530

    Build a compact invoice using rest

---
+++ Assignment/JavaScript function loops objects JSON Assg-2 Build a Compact Invoice Using Rest + Default.js
@@ -0,0 +1,30 @@
+function invoice(gstRate = 0.18, ...items) {
+
+    let subtotal = 0;
+
+    for (let i = 0; i < items.length; i++) {
+
+        let item = items[i];
+
+        // Stop if item name is STOP
+        if (item.name === "STOP") {
+            break;
+        }
+
+        // Ignore invalid items
+        if (!item.price || !item.qty || item.price <= 0 || item.qty <= 0) {
+            continue;
+        }
+
+        subtotal += item.price * item.qty;
+    }
+
+    let gst = subtotal * gstRate;
+    let total = subtotal + gst;
+
+    return {
+        subtotal: subtotal,
+        gst: gst,
+        total: total
+    };
+}
