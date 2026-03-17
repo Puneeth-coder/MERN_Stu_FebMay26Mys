f241f671d738e0edf0c2dc38e8f85fb7dd0694f1
Puneeth-coder <puneethm240@gmail.com>
2026-03-17 16:20:33 +0530

    Chaining promises with centralised error handling

---
+++ W5/D2/P12.js
@@ -0,0 +1,32 @@
+//Chaining promises with centralised error handling
+function validateLogin(){
+    return new Promise(function(resolve){
+        setTimeout(function(){
+            resolve("Login Validate");
+        },500)
+    })
+}
+function fetchAccountData(){
+    return new Promise(function(resolve,reject){
+        setTimeout(function(){
+            const accountFound = true;
+            if(accountFound){
+            resolve("Account Data fount");
+            }
+            else{
+                reject("Account Data not Found");
+            }
+        },700)
+    })
+}
+validateLogin()
+.then(function(message){
+    console.log(message);
+    return fetchAccountData();
+})
+.then(function(accountMessage){
+    console.log(accountMessage);
+})
+.catch(function(error){
+    console.log("Chain error:",error);
+});
