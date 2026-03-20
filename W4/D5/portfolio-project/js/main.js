acee6653884bff459324fbe8ad21ac04a009d035
Puneeth-coder <puneethm240@gmail.com>
2026-03-20 10:01:59 +0530

    Adding main js file containing other file function

---
+++ W4/D5/portfolio-project/js/main.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded",function(){
+    // render skills
+    renderSkills();
+    initModal();
+    initContactValidation();
+    initThemeToggle();
+    renderProject();
+    scrollInit();
+    welcomeInit();
+    letsPopInit();
+});
