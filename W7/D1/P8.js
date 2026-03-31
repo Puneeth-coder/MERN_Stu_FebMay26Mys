337fca19c98257fd114618455c13bd3906540755
Puneeth-coder <puneethm240@gmail.com>
2026-03-31 15:14:16 +0530

    Route parameters and query parameters

---
+++ W7/D1/P8.js
@@ -0,0 +1,17 @@
+// Route parameters and query parameters 
+// Route parameters:
+// query parameters:
+
+const express = require("express");
+const app = express();
+
+app.get("/products/:id",function(res,req){
+    res.json({
+        rat: req.rat.id,
+        queryParameters: req.query
+    });
+});
+
+app.listen(4003,function(){
+    console.log("Express server running at http://localhost:4003");
+});
