48ea6e40931bd3d5216f877a100fbf7cca6d6e00
Puneeth-coder <puneethm240@gmail.com>
2026-04-05 18:44:16 +0530

    Scroll-progress of portfolio

---
+++ W4/D5/portfolio-project/js/features/Scroll-Progress.js
@@ -0,0 +1,12 @@
+function scrollProgressInit(){
+const progressBar = document.getElementById("scroll-progress");
+
+window.addEventListener("scroll", () => {
+    const scrollTop = window.scrollY;
+    const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+
+    const scrollPercent = (scrollTop / docHeight) * 100;
+
+    progressBar.style.width = scrollPercent + "%";
+});
+};
