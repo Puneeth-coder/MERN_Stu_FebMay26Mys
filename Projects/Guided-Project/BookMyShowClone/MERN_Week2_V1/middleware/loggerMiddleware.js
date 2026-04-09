1d0f7b63a1e68d09e8553e0b1a78cdc9b47e54b5
Puneeth-coder <puneethm240@gmail.com>
2026-04-09 23:52:26 +0530

    Renamed folder

--- Projects/Guided-Project/BookMyShowClone/MERN_Week2_V1/middleware/loggerMiddleware.js
+++
@@ -1,7 +0,0 @@
-// Middleware for req log
-function loggerMiddleware(req,res,next){
-    console.log(`${req.method} ${req.originalURl}`);
-    next();
-}
-module.exports = loggerMiddleware;
-// Task- write the log to a file along with timestamp with both req and res
