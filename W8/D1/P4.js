5f813e512b03e0524cce9771af9dd0024215fa4b
Puneeth-coder <puneethm240@gmail.com>
2026-04-11 09:49:52 +0530

    Sorting, limiting, and pagination

--- W8/D1/P4.js
+++ W8/D1/P4.js
@@ -8,7 +8,22 @@ async function sortPaginationDemo() {
     console.log("MongoDB connected successfully");
 
     const sortedAccending = await Product.find({catagory: "Electronics"}).sort({price:1});
-    console.log("Sorted Accending:",sortedAccending);
+    // console.log("Sorted Accending:",sortedAccending);
+
+    const sortedDccending = await Product.find({catagory: "Electronics"}).sort({price:-1});
+    // console.log("Sorted Dccending:",sortedDccending);
+
+    // limit() retricts count 
+    const limitedResults = await Product.find({catagory:"Stationary"}).sort({createdOrder:1}).limit(2);
+        // console.log("Limited Results:",limitedResults);
+
+        // Pagination
+        const page = 2;
+        const limitCount = 2;
+        const skipCount = (page-1)*limitCount;
+
+        const paginatedResults = await Product.find({catagory:"Stationary"}).sort({createdOrder:1}).skip(skipCount).limit(limitCount);
+        console.log("Pagination results:",paginatedResults)
 
     await mongoose.connection.close();
             console.log(("Connection closed"));
