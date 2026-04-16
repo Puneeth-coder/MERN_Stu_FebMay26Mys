7afd085abfb319774fe65a0486ce1813ec70a002
Puneeth-coder <puneethm240@gmail.com>
2026-04-16 11:49:51 +0530

    OTP generation using crypto is built-in module of Node.js

---
+++ W8/D4/P4.js
@@ -0,0 +1,7 @@
+// OTP generation using crypto is built-in module of Node.js
+const crypto = require("crypto");
+
+function generateOTP(length = 6){
+    return crypto.randomInt(0,10**length).toString().padStart(length,'0');
+} 
+console.log("OTP:",generateOTP());
