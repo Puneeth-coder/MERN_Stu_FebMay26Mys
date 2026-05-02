c2d13e88ef7a648e467967c6e9f948febdfbb642
Puneeth-coder <puneethm240@gmail.com>
2026-05-02 19:06:24 +0530

    DB connection function invoked

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/server.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/server.js
@@ -1,11 +1,11 @@
-// Load the details from the environment valiables
+// Load environment variables
 require("dotenv").config();
 
 const app = require("./app");
-const connectDB = require("./src/config/db");
+const connectDB  = require("./src/config/db");
 // Connect DB
 connectDB();
-// Port configuration 
+// Port config
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT,()=>{
