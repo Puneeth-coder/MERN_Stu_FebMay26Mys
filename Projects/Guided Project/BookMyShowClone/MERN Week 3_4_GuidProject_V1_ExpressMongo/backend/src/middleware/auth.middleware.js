06ff97c8178a941a8ed8e304765b2277f06fab46
Puneeth-coder <puneethm240@gmail.com>
2026-05-02 18:48:30 +0530

    Modified

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/middleware/auth.middleware.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/middleware/auth.middleware.js
@@ -1,46 +1,97 @@
 const jwt = require("jsonwebtoken");
-const user = require("../models/User");
 const User = require("../models/User");
 
 // Auth middleware 
-exports.protect = async(req,resizeBy,next)=>{
+exports.protect = async(req,res,next)=>{
     try
     {
+// console.log("running 0");
+
         let token;
         if(
             req.headers.authorization && 
             req.headers.authorization.startsWith("Bearer")
         ){
-            token = req.authorization.split(" ")[1];
+            token = req.headers.authorization.split(" ")[1];
+// console.log("running 0 .1");
         }
+        console.log("token : ",token);
+        
         if(!token){
-            return resizeBy.status(401).json({
+// console.log("running 0 .2");
+
+            return res.status(401).json({
                 success:false,
                 message:"Not authorized, token missing"
             });
+        }
+// console.log("running 1");
 
             // Verify token
-            const decoded = jwt.verify(token,procrss.env.JWT_SECRET);
+            const decoded = jwt.verify(token,process.env.JWT_SECRET);
 
             // Get the user from DB
-            const user = await User.findById(decode.id);
+            const user = await User.findById(decoded.id);
 
             if(!user){
-                 return resizeBy.status(404).json({
+                 return res.status(404).json({
                   success:false,
                   message:"User not found"
                 });
            }
+// console.log("running 2");
 
              // atach the user to request 
              req.user = user;
              next();
-        }
     }
     catch(error){
-        return resizeBy.status(401).json({
+        return res.status(401).json({
                   success:false,
                   message:"Invalid or expired token"
                 });
     }
 }
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+// Auth middleware
+exports.protect = async(req,res,next)=>{
+    try{
+        let token;
+        if(
+            req.headers.authorization && 
+            req.headers.authorization.startsWith("Bearer")
+        ){
+            token = req.headers.authorization.split(" ")[1];
+        }
+        if(!token){
+            return res.status(401).json({
+                success:false,
+                message:"Not authorized, token missing",
+            });
+        }
+
+        // Verify token
+        const decoded = jwt.verify(token,process.env.JWT_SECRET);
+
+        // Get user from DB
+        const user = await User.findById(decoded.id);
+
+        if(!user){
+            return res.status(404).json({
+                success:false,
+                message:"User not found",
+            });
+        }
+        // Attach user to request
+        req.user = user;
+        next();
+    }
+    catch(error){
+        return res.status(401).json({
+            success:false,
+            message:"Invalid or expired token"
+        });
+    }
+};
