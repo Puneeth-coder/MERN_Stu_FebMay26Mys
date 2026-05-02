ff3a587e6dfa1046b943cdc7d56290f09ccd074a
Puneeth-coder <puneethm240@gmail.com>
2026-05-02 18:27:04 +0530

    Added ClassComponentState component

---
+++ W9/D2/my-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+// import  {FunctionalComponentsBasics} from './components/FunctionalComponentsBasics'
+// import  {FunctionComp} from './components/FunctionalComponentAdv'
+import {ClassComponentState} from './components/ClassComponentState'
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <ClassComponentState/>
+    </>
+  )
+}
+
+export default App
