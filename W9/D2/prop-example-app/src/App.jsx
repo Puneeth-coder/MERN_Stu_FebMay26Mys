7f8aefe42e70214d3a0ef1544ffbc67940b8e056
Puneeth-coder <puneethm240@gmail.com>
2026-05-02 18:30:43 +0530

    Props basics react app created

---
+++ W9/D2/prop-example-app/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+// import { PropBasics } from '../components/P1'
+// import { PropDestructuring } from '../components/P2'
+// import { PropsChildren } from '../components/P3'
+// import { PropDrillingDemo } from '../components/P4'
+// import { SharedStateParent } from '../components/P5'
+// import { CompositionDemo } from '../components/P6'
+// import { ObjectsAndArrayProps } from '../components/P7'
+// import { PropsImmutabilty } from '../components/P8'
+// import { PropsState } from '../components/P9'
+// import { FunctionProps } from '../components/P10'
+// import { PropTypesDemo } from '../components/P11'
+import { HOCExample } from './components/P12'
+
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      {/* <PropBasics/> */}
+      {/* <PropDestructuring/> */}
+      {/* <PropsChildren/> */}
+      {/* <PropDrillingDemo/> */}
+      {/* <SharedStateParent/> */}
+      {/* <CompositionDemo/> */}
+      {/* <ObjectsAndArrayProps/> */}
+      {/* <PropsImmutabilty/> */}
+      {/* <PropsState/> */}
+      {/* <FunctionProps/> */}
+      {/* <PropTypesDemo/> */}
+      <HOCExample/>
+    </>
+  )
+}
+
+export default App
