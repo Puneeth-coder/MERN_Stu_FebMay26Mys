3b8c99ca5d8174132ae172b248ab64425097f5fa
Puneeth-coder <puneethm240@gmail.com>
2026-06-12 11:36:50 +0530

    Single  Component, Component Composition, parent-child components.

---
+++ W12/Final Frontend Project/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import HomePage from "./pages/HomePage";
+function App() {
+  return (
+    // Component Composition :
+    //  multiple files are puted in to one page called Component Composition
+      <HomePage/>
+  );
+}
+
+export default App
