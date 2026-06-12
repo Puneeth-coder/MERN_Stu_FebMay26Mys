da4e984037bb9b68e53bad81816002d05a9c4d10
Puneeth-coder <puneethm240@gmail.com>
2026-06-12 12:04:33 +0530

    Usage of props

--- W12/Final Frontend Project/src/App.jsx
+++ W12/Final Frontend Project/src/App.jsx
@@ -1,11 +1,34 @@
 import './App.css'
-import HomePage from "./pages/HomePage";
+import MovieCard from "./components/MovieCard";
 function App() {
-  return (
+    return (
     // Component Composition :
     //  multiple files are puted in to one page called Component Composition
-      <HomePage/>
-  );
+    <>
+    <MovieCard
+        title="Inception"
+        genre="Sci-fi"
+        rating={4.8}
+        duration="2h 28m"
+        poster="https://picsum.photos/200/300?1"
+    />
+          <MovieCard
+        title="Avatar"
+        genre="Sci-fi"
+        rating={4.1}
+        duration="3h 28m"
+        poster="https://picsum.photos/200/300?2"
+         /> 
+         <MovieCard
+        title="FightClub"
+        genre="Sci-fi"
+        rating={4.9}
+        duration="2h 28m"
+        poster="https://picsum.photos/200/300?3"
+         />
+    </>
+      
+    );
 }
 
 export default App
